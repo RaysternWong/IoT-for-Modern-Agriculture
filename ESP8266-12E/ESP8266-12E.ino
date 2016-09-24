46f8dd6ee0e70d063715517cfb8040f3dd062c73
Raydivine <raydivine00@gmail.com>
2016-09-24 19:43:47 +0800

    Written function that write data to field 1

--- ESP8266-12E/ESP8266-12E.ino
+++ ESP8266-12E/ESP8266-12E.ino
@@ -1,9 +1,19 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 
+#define PIN1 1
+#define PIN2 2
+#define PIN3 3
+
+WiFiClient client;
+
+String apiWrite = "DRW5K6F91WT954LA"; //The Key for write data to channel
+String apiRead  = "QW4PODDM2Q96LA24"; //The Key for read data from channel
+
 const char* ssid = "familywong88";
 const char* password = "72680384";
- 
+const char* server = "api.thingspeak.com";
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -26,3 +36,30 @@ void setup() {
 void loop() { 
 
 }
+
+void writeToField1(float data){
+  if (client.connect(server,80)) {  //   "184.106.153.149" or api.thingspeak.com
+    String postStr = apiWrite;
+           postStr +="&field1=";
+           postStr += String(data);
+           postStr += "\r\n\r\n";
+ 
+     client.print("POST /update HTTP/1.1\n"); 
+     client.print("Host: api.thingspeak.com\n"); 
+     client.print("Connection: close\n"); 
+     client.print("X-THINGSPEAKAPIKEY: "+apiWrite+"\n"); 
+     client.print("Content-Type: application/x-www-form-urlencoded\n"); 
+     client.print("Content-Length: "); 
+     client.print(postStr.length()); 
+     client.print("\n\n"); 
+     client.print(postStr);
+           
+  }
+  client.stop();
+}
+
+int readSignalFrom(int pinNum){
+  
+}
+
+
