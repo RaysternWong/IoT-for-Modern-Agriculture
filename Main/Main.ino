497b36cba7eeae9d515c963d89ec00646f6cb5e7
Raydivine <raydivine00@gmail.com>
2016-10-11 18:36:04 +0800

    included DHT11 library

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
+#include "DHT.h"
 #include "Server.h"
 #include "Network.h"
 
@@ -15,7 +16,7 @@
 #define D9  3
 #define D10 1
 
-#define WRITE_DATA_TO_FILED1(data)  writeDataToThingSpeak(data, apiWrite, "field1")
+#define BAUDRATE 115200
 
 String apiWrite = "DRW5K6F91WT954LA"; //The Key for write data to channel
 String apiRead  = "QW4PODDM2Q96LA24"; //The Key for read data from channel
@@ -25,12 +26,12 @@ const char* password = "72680384";
 
 //const char* ssid = "TARUC Wireless";
 //const char* password = "";
+//const char* server = "api.thingspeak.com";
 
- //const char* server = "api.thingspeak.com";
-
+DHT dht(D8, DHT11,15);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(BAUDRATE);
   delay(10);
 
   Serial.println("\n");
@@ -42,39 +43,8 @@ void loop() {
   data1 = digitalRead(D6);
   Serial.println(data1);
   
-  WRITE_DATA_TO_FILED1(data1);
+
   
   delay(2000);  
-  /*
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(12);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
-  Serial.print("V");
 
-  if (client.connect(server,80)) {  //   "184.106.153.149" or api.thingspeak.com
-    String postStr = apiWrite;
-           postStr +="&field1=";
-           postStr += String(voltage);
-           postStr += "\r\n\r\n";
- 
-     client.print("POST /update HTTP/1.1\n"); 
-     client.print("Host: api.thingspeak.com\n"); 
-     client.print("Connection: close\n"); 
-     client.print("X-THINGSPEAKAPIKEY: "+apiWrite+"\n"); 
-     client.print("Content-Type: application/x-www-form-urlencoded\n"); 
-     client.print("Content-Length: "); 
-     client.print(postStr.length()); 
-     client.print("\n\n"); 
-     client.print(postStr);
-             
-  }
-  client.stop();
-   
-  Serial.println("Waiting...");    
-  // thingspeak needs minimum 15 sec delay between updates
-  delay(20000);  
-  */
 }
