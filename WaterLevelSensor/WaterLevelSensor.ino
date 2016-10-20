499577cd3c127234fcf6eeacf2c6d032761a8a9a
Raydivine <raydivine00@gmail.com>
2016-10-20 16:13:39 +0800

    changed way to read water level

--- WaterLevelSensor/WaterLevelSensor.ino
+++ WaterLevelSensor/WaterLevelSensor.ino
@@ -16,9 +16,38 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
+  int waterHeight;
   int sensorValue = analogRead(A0);
+/*
+  if(sensorValue>690)
+    waterHeight = map(sensorValue, 700, 1024, 0, 40);
+  else
+    waterHeight = 0;
   // print out the value you read:
+*/
+  if(sensorValue>700)
+    waterHeight = 1;
+  if(sensorValue>736)
+    waterHeight = 5;
+  if(sensorValue>790)
+    waterHeight = 10;
+  if(sensorValue>827)
+    waterHeight = 15;
+  if(sensorValue>843)
+    waterHeight = 20;
+  if(sensorValue>857)
+    waterHeight = 25;
+  if(sensorValue>870)
+    waterHeight = 3;
+  if(sensorValue>910)
+    waterHeight = 3.5;
+  if(sensorValue>1010)
+    waterHeight = 40;
+
+  
   Serial.println(sensorValue);
+  Serial.println(waterHeight);
+
   Serial.println();
   delay(1000);        // delay in between reads for stability
 }
